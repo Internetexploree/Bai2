0de29fdb86d99631f7eb54d5855116b34a55a183
Internetexploree <lequi1322@gmail.com>
2023-03-24 00:09:35 +0700

    Add project files.

---
+++ Bai2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap chieu dai: ");
+            float chieuDai = float.Parse(Console.ReadLine());
+            Console.Write("Nhap chieu rong: ");
+            float chieuRong = float.Parse(Console.ReadLine());
+
+            float dienTich = chieuDai * chieuRong;
+            float chuVi = (chieuDai + chieuRong) * 2;
+            Console.WriteLine("Dien tich cua hinh chu nhat la: {0}", dienTich);
+            Console.WriteLine("Chu vi cua hinh chu nhat la: {0}", chuVi);
+            Console.ReadKey();
+        }
+    }
+}
